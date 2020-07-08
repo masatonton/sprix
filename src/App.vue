ab887cf2e86da5e6712d9ead2d8431170d4d6c7b
masatonton <kakerunrun100@gmail.com>
2020-07-09 01:54:08 +0900

    “あはは”

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+        <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-title class="title">
+          Sprix
+        </v-list-item-title>
+        <v-btn icon>
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-list-item>
+      <v-divider />
+      <v-list nav>
+        <v-list-item v-for="menu in menus" :key="menu.title" :to="menu.url">
+          <v-list-item-icon>
+            <v-icon>{{ menu.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ menu.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app dark>
+      <!-- アプリケーションバー -->
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Sprix</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <Navigation/>
+      <Carousels/> <br><br><br>
+      <h2>Events</h2><br>
+      <Cards/> <br>
+      <Cards2/><br><br>
+      <v-divider></v-divider> <br><br>
+      <h2 style="" >About Sprix</h2><br>
+      <Footer/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Carousels from './components/Carousels';
+import Cards from './components/Cards';
+import Cards2 from './components/Cards2';
+import Footer from './components/Footer';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    Carousels,
+    Cards,
+    Cards2,
+    Footer,
+  },
+
+    data: () => ({
+    drawer: false,
+    menus: [
+      { title: 'Event', icon: 'mdi-web', url: '#events' },
+      { title: 'About', icon: 'mdi-home', url: '#about' },
+      { title: 'How to Entry', icon: 'mdi-heart', url: '#How' },
+      { title: 'Contact', icon: 'mdi-information-variant', url:'#contact'}
+    ]
+  })
+} 
+  
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped>
+</style>>
---
+++ src/components/Cards.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      src="../assets/coecuEgi.png"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      メソロギア
+    </v-card-title>
+
+    <v-card-subtitle>
+      Vo1. Methologia Chapionship
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn text>SHARE</v-btn>
+
+      <v-btn
+        color="purple"
+        text
+      >
+        詳細/エントリーページ
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          <h3>概要：</h3>
+          <p>日程：8/2</p>
+          <p>賞金<p/>
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      show: false,
+    }),
+  }
+
+  
+</script>
+
