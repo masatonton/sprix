35ad2cd217804bf037d788fc24f3e7822da396bc
masatonton <kakerunrun100@gmail.com>
2020-08-12 22:58:55 +0900

    team

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -8,12 +8,40 @@
             dark
           >
             <v-card-title subtitle-1 class="headline">
-              運営
+              Sprix.1
             </v-card-title>
             
 
-            <v-card-subtitle>Sprixは主に個人開発ゲームアプリの大会告知/運営を行っています。個人大会の主催なども掲載いたしますので、掲載ご希望の方はお問い合わせください。</v-card-subtitle>
+            <v-card-subtitle>Sprixは個人開発ゲームの大会運営、広報を行っております。掲載をご希望の方はお問い合わせください。料金はかかりません。</v-card-subtitle>
           </v-card>
+          <br>
+
+          <v-card
+            color="purple darken-0"
+            max-width="80vw;"
+            dark
+          >
+            <v-card-title subtitle-1 class="headline">
+              Sprix.2
+            </v-card-title>
+            
+
+            <v-card-subtitle>参加費無料/賞品有りの大会のみの掲載となります。掲載をご希望の開発者様は賞品のみのご用意をよろしくお願いします。</v-card-subtitle>
+          </v-card>
+          <br>
+          <v-card
+            color="green darken-0"
+            max-width="80vw;"
+            dark
+          >
+            <v-card-title subtitle-1 class="headline">
+              Sprix.3
+            </v-card-title>
+            
+
+            <v-card-subtitle>運営は全てボランティアで行っております。トーナメントディレクター経験のある者が運営を行いますが、基本1人で運営を行いますのでスムーズな大会進行のためにもご参加される方は予め大会詳細ページを一読下さい。分からないところがある場合はお気軽にTwitterアカウントのDMまでお問い合わせください。よろしくお願いいたします。</v-card-subtitle>
+          </v-card>
+
         </v-col>
       </v-row>
     </v-container>
